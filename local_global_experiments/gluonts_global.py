2a4624558a9ab40de805dbc10ea563e14ae7149c
Thomas Guyet <thomas.guyet@inria.fr>
2022-02-23 17:27:35 +0100

    add some elements for global approach of gluonts

--- local_global_experiments/gluonts_global.py
+++ local_global_experiments/gluonts_global.py
@@ -10,6 +10,7 @@ import mxnet as mx
 from mxnet import gluon
 import numpy as np
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
 
 from gluonts.dataset.common import ListDataset
@@ -31,64 +32,90 @@ freq='1D'
 
 nb_series=len(data.bss.drop_duplicates())
 
-print("prepare dataset")
-# train dataset made of all the time series
-train_ds = ListDataset(
-    [{'target': data[data.bss==bss_id].p[:-prediction_length].to_numpy(), 'start': '2015-01-01'} for bss_id in data.bss.drop_duplicates()],
-    freq=freq
-)
-# test dataset
-test_ds = ListDataset(
-    [{'target': data[data.bss==bss_id].p.to_numpy(), 'start': '2015-01-01'} for bss_id in data.bss.drop_duplicates()],
-    freq=freq
-)
+print('pre-compute TN')
+TN=[{'bss':bss_id,'TN':np.sum((data[data.bss==bss_id].iloc[1:-prediction_length].p.to_numpy()-data[data.bss==bss_id].iloc[:-prediction_length-1].p.to_numpy())**2)} for bss_id in data.bss.drop_duplicates()]
+TN=pd.DataFrame(TN)
 
-
-print("learn model")
-"""
-estimator = SimpleFeedForwardEstimator(
-    num_hidden_dimensions=[10],
-    prediction_length=prediction_length,
-    context_length=100,
-    freq=freq,
-    trainer=Trainer(
-        ctx="cpu",
-        epochs=150,
-        learning_rate=1e-3,
-        num_batches_per_epoch=32
+metrics_list=[]
+#for covariates in [[] ,['tp'],['e'], ['tp','e']]:
+for covariates in [[],['tp'],['e'], ['tp','e']]:
+    print("prepare dataset with covariates: "+ ', '.join(covariates) +".")
+    # train dataset made of all the time series
+    train_ds = ListDataset(
+        [{'target': data[data.bss==bss_id].p[:-prediction_length].to_numpy(), 
+          'start': '2015-01-01', 
+          'feat_dynamic_real':data[data.bss==bss_id][covariates][:-prediction_length]} for bss_id in data.bss.drop_duplicates()],
+        freq=freq
     )
-)
-"""
-estimator = DeepAREstimator(
-    prediction_length=prediction_length,
-    context_length=100,
-    freq=freq,
-    trainer=Trainer(
-        ctx="cpu",
-        epochs=20,
-        learning_rate=1e-3,
-        num_batches_per_epoch=32
+    # test dataset
+    test_ds = ListDataset(
+        [{'target': data[data.bss==bss_id].p.to_numpy(), 
+          'start': '2015-01-01',
+          'feat_dynamic_real':data[data.bss==bss_id][covariates]} for bss_id in data.bss.drop_duplicates()],
+        freq=freq
     )
-)
+    
+    
+    print("learn model")
+    """
+    estimator = SimpleFeedForwardEstimator(
+        num_hidden_dimensions=[10],
+        prediction_length=prediction_length,
+        context_length=100,
+        freq=freq,
+        trainer=Trainer(
+            ctx="cpu",
+            epochs=150,
+            learning_rate=1e-3,
+            num_batches_per_epoch=32
+        )
+    )
+    """
+    estimator = DeepAREstimator(
+        prediction_length=prediction_length,
+        context_length=100,
+        freq=freq,
+        trainer=Trainer(
+            ctx="cpu",
+            epochs=30,
+            learning_rate=1e-3,
+            num_batches_per_epoch=32
+        )
+    )
+    
+    #learn the predictor
+    start = time.time()
+    predictor = estimator.train(train_ds)
+    end = time.time()
+    learning_time = end - start
+    
+    
+    print("forecast")
+    #do forecast
+    forecast_it, ts_it = make_evaluation_predictions(
+        dataset=test_ds,  # test dataset
+        predictor=predictor,  # predictor
+        num_samples=100,  # number of sample paths we want for evaluation
+    )
+    
+    # evaluate the results
+    forecasts = list(forecast_it)
+    tss = list(ts_it)
+    
+    evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
+    agg_metrics, item_metrics = evaluator(iter(tss), iter(forecasts), num_series=nb_series)
+    item_metrics.item_id = data.bss.drop_duplicates().reset_index(drop=True)
+    
+    item_metrics['model']=estimator.__class__.__name__
+    item_metrics['learningtime']=learning_time
+    item_metrics['use_exo_rain']=('tp'in covariates)
+    item_metrics['use_exo_evo']=('e'in covariates)
+    item_metrics['rmse']=np.sqrt(item_metrics['MSE'])
+    item_metrics=pd.merge(item_metrics, TN, left_on="item_id", right_on="bss").drop("bss",axis=1)
+    item_metrics['rmsse']=np.sqrt(item_metrics['MSE']/item_metrics['TN'])
+    metrics_list.append(item_metrics)
 
-#learn the predictor
-predictor = estimator.train(train_ds)
+    pd.concat(metrics_list).to_csv(estimator.__class__.__name__+"_global.csv.tmp")
 
-
-print("forecast")
-#do forecast
-forecast_it, ts_it = make_evaluation_predictions(
-    dataset=test_ds,  # test dataset
-    predictor=predictor,  # predictor
-    num_samples=100,  # number of sample paths we want for evaluation
-)
-
-# evaluate the results
-forecasts = list(forecast_it)
-tss = list(ts_it)
-
-evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
-agg_metrics, item_metrics = evaluator(iter(tss), iter(forecasts), num_series=nb_series)
-item_metrics.item_id = data.bss.drop_duplicates().reset_index(drop=True)
-
-print(item_metrics)
+metrics = pd.concat(metrics_list)
+metrics.to_csv(estimator.__class__.__name__+"_global.csv")
